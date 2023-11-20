527dca23c42260ea8ee33754af9278b2f8408c92
Jason Maliquid <jasonm2822@gmail.com>
2023-11-20 09:34:21 +0800

    Updated Index

---
+++ script/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function hanleButtonClick(event) {
+    event.preventDefault();
+  }
+
+  const loginBtn = document.getElementById("loginbtn");
+  loginBtn.addEventListener("click", function (event) {
+    window.location.href = "login.html";
+  });
+
+  const signupBtn = document.getElementById("signupbtn");
+  signupBtn.addEventListener("click", function (event) {
+    window.location.href = "signupstep1.html";
+  });
+
+  const searchBtn = document.getElementById("searchbtn");
+  searchBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.href = "https://www.pornhub.com";
+  });
+});
